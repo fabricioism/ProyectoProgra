8c39efd32b355f8e700abb46cac7c8f661f33035
ChristhianFlores <fchristhian@yahoo.es>
2016-10-30 20:08:33 -0600

    Update Juego.java

--- Projecto Programacion/src/principal/Juego.java
+++ Projecto Programacion/src/principal/Juego.java
@@ -1,9 +1,6 @@
 package principal;
 
-import java.awt.BufferCapabilities;
 import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -18,31 +15,36 @@ import javax.swing.JFrame;
 import medios.Jugador;
 
 
-public class Juego extends Canvas {
+public class Juego extends Canvas implements KeyListener {
 	private JFrame ventana;
 	private BufferStrategy dobleBuffer;
 	private Graphics2D g2D;
 
 	private boolean jugando  = false;
 
-	public static final int ANCHO_VENTANA = 700;
-	public static final int ALTO_VENTANA = 500;
+	public static final int ancho =500;
+	public static final int alto = 500;
 
 	int lastFpsTime; 
 	int fps; 
+	private Jugador jugador;
 		
-	public static HashMap<String,BufferedImage> terreno = new HashMap<String,BufferedImage>();
+	public static HashMap<String,BufferedImage> imagenes = new HashMap<String,BufferedImage>();
 	//public static HashMap<String,BufferedImage> orillas = new HashMap<String, BufferedImage>();
-
+	public static boolean izquierda;
+	public static boolean derecha;
+	public static boolean arriba;
+	public static boolean abajo;
 	public Juego(){
 		cargarImagenes();
-
+		inicializarObjetos();
 		incializarVentana();
 
 		createBufferStrategy(2); 
 		dobleBuffer = getBufferStrategy(); 
 		jugando = true;
-
+		this.requestFocus(); 
+		this.addKeyListener(this);
 		cicloPrincipal();
 	}
 
@@ -50,13 +52,17 @@ public class Juego extends Canvas {
 		
 		ventana = new JFrame(); 
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		ventana.setSize(ANCHO_VENTANA, ALTO_VENTANA); 
+		ventana.setSize(ancho, alto); 
 		ventana.setLocationRelativeTo(null); 
 		ventana.setTitle("Establezca el titulo de su conveniencia"); 
 		ventana.getContentPane().add(this); 
 		ventana.setVisible(true); 
 	}
 
+	public void inicializarObjetos(){
+		
+		jugador=new Jugador("yo", 250, 250, 3, "caminandoI");
+	}
 
 
 
@@ -65,9 +71,12 @@ public class Juego extends Canvas {
 		try {
 			BufferedImage superficie = ImageIO.read(getClass().getResource("/imagenes/terreno.jpg"));
 			BufferedImage bordes = ImageIO.read(getClass().getResource("/imagenes/orillas.jpg"));
-			terreno.put("cesped", superficie.getSubimage(0, 0,192, 192));
-			terreno.put("lava", superficie.getSubimage(192, 0, 192, 192));
-			terreno.put("orillaH", bordes.getSubimage(32, 128, 32, 32));
+			BufferedImage hertz = ImageIO.read(getClass().getResource("/imagenes/hertz.png"));
+			imagenes.put("caminandoI", hertz.getSubimage(0, 0, 20, 39));
+			imagenes.put("cesped", superficie.getSubimage(0, 0,192, 192));
+			imagenes.put("lava", superficie.getSubimage(192, 0, 192, 192));
+			imagenes.put("orillaH", bordes.getSubimage(32, 128, 32, 32));
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -77,17 +86,23 @@ public class Juego extends Canvas {
 
 	private void pintar(){
         g2D = (Graphics2D)dobleBuffer.getDrawGraphics(); 
-     
-        			g2D.drawImage(terreno.get("cesped"), 30, 30, this);
-        			g2D.drawImage(terreno.get("lava"), 50, 50, this);
-        			g2D.drawImage(terreno.get("orillaH"), 50, 50, this);
+        for (int i=0; i<ancho; i++){
+        	g2D.drawImage(imagenes.get("cesped"), 32*i, 0, this);
+        	g2D.drawImage(imagenes.get("cesped"),32*i,192,this);
+        	g2D.drawImage(imagenes.get("cesped"),32*i,384,this);
+        }
+        			/*g2D.drawImage(terreno.get("lava"), 50, 50, this);
+        			g2D.drawImage(terreno.get("orillaH"), 50, 50, this);*/
+        //g2D.drawImage(imagenes.get("caminandoI"),10 ,10 , this);
 
 
-
+        jugador.pintar(g2D, this);
         dobleBuffer.show(); 	}
 
 	
-
+	public void actualizar(){
+		jugador.mover();
+	}
 	public void cicloPrincipal(){
 	   long lastLoopTime = System.nanoTime();
        final int TARGET_FPS = 60;
@@ -106,6 +121,7 @@ public class Juego extends Canvas {
     		   fps = 0;
     	   }
     	   pintar();
+    	   actualizar();
     	   try{Thread.sleep((lastLoopTime-System.nanoTime() + OPTIMAL_TIME)/1000000 );} //Puede sustituir el valor de la pausa por un valor fijo
     	   catch(Exception e){};
        }
@@ -114,5 +130,51 @@ public class Juego extends Canvas {
     public static void main(String[] args){
 		new Juego(); 	
 		}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		 switch(arg0.getKeyCode()){
+     	case KeyEvent.VK_LEFT:
+     		izquierda=true;
+     		break;
+     	case KeyEvent.VK_RIGHT:
+     		derecha=true;
+     		break;
+     	case KeyEvent.VK_UP:
+     		arriba=true;
+     		break;
+     	case KeyEvent.VK_DOWN:
+     		abajo=true;
+     		break;
+		
+	}
+		 
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		 switch(arg0.getKeyCode()){
+	        case KeyEvent.VK_LEFT:
+	    		izquierda=false;
+	    		break;
+	    	case KeyEvent.VK_RIGHT:
+	    		derecha=false;
+	    		break;
+	    	case KeyEvent.VK_UP:
+	    		arriba=false;
+	    		break;
+	    	case KeyEvent.VK_DOWN:
+	    		abajo=false;
+	    		break;
+     }
+
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
     
 }
